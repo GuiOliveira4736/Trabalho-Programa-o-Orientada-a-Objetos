3b61e1a82547c71f091f7de4a154d9cc6a5af49e
GuiOliveira4736 <86176297+GuiOliveira4736@users.noreply.github.com>
2023-12-10 05:27:48 -0300

    Versão 10_12_23

---
+++ ProjetoSistema/src/projetosistema/App.java
@@ -0,0 +1,29 @@
+
+package projetosistema;
+import java.io.Serializable;
+
+public class App implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private String descricao;
+    private String link;
+
+    public App(String nome, String descricao, String link) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.link = link;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getLink() {
+        return link;
+    }
+}
--- ProjetoSistema/src/projetosistema/ProjetoSistema.java
+++ ProjetoSistema/src/projetosistema/ProjetoSistema.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package projetosistema;
 
 /**
@@ -10,11 +7,9 @@ package projetosistema;
  */
 public class ProjetoSistema {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
